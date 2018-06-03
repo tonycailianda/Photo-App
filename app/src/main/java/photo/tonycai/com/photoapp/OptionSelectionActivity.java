4cd16ef8c7752c586859355e0714a346e1b748ce
Tony Cai <cailiandatony@gmail.com>
2018-06-03 11:17:13 +0800

    Haha

--- app/src/main/java/photo/tonycai/com/photoapp/OptionSelectionActivity.java
+++ app/src/main/java/photo/tonycai/com/photoapp/OptionSelectionActivity.java
@@ -13,10 +13,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.Switch;
 
-public class OptionSelectionActivity extends AppCompatActivity {
+public class OptionSelectionActivity extends AppCompatActivity{
     private static int RESULT_LOAD_IMAGE = 1;
+    private int ShowResultActivity = 1;
+    int rotation_data= 0;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,8 +38,29 @@ public class OptionSelectionActivity extends AppCompatActivity {
         });
 
     }
+    SeekBar.OnSeekBarChangeListener SeekBarListener = new SeekBar.OnSeekBarChangeListener()
+    {
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress,
+                                      boolean fromUser) {
+            // TODO Auto-generated method stub
+            rotation_data=progress;
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            // TODO Auto-generated method stub
+        }
+    };
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
@@ -56,17 +80,25 @@ public class OptionSelectionActivity extends AppCompatActivity {
             Switch sc = (Switch) findViewById(R.id.switch_function_a);
             Switch sd = (Switch) findViewById(R.id.switch_function_b);
 
+            SeekBar seekBar = findViewById(R.id.rotation_seekbar);
+
+            seekBar.setOnSeekBarChangeListener(SeekBarListener);
             Intent myIntent = new Intent(OptionSelectionActivity.this, ShowResultActivity.class);
-            myIntent.putExtra("is_filter_a", sa.isChecked()); //Optional parameters
+            myIntent.putExtra("is_filter_a",sa.isChecked()); //Optional parameters
             myIntent.putExtra("is_filter_b", sb.isChecked()); //Optional parameters
             myIntent.putExtra("is_function_a", sc.isChecked()); //Optional parameters
             myIntent.putExtra("is_function_b", sd.isChecked()); //Optional parameters
-            startActivity(myIntent);
+            myIntent.putExtra("picPath", picturePath); //Optional parameters
+            myIntent.putExtra("rotation_angle", rotation_data); //Optional parameters
+            startActivityForResult(myIntent,ShowResultActivity);
 //            Bitmap imageSource = BitmapFactory.decodeFile(picturePath);
 //            ImageFilters filters = new ImageFilters();
 //            Bitmap afterFilter = filters.applyInvertEffect(imageSource);
 //            ImageView imageView = (ImageView) findViewById(R.id.imageView);
 //            imageView.setImageBitmap(afterFilter);
         }
+
     }
+
+
 }
--- app/src/main/java/photo/tonycai/com/photoapp/ShowResultActivity.java
+++ app/src/main/java/photo/tonycai/com/photoapp/ShowResultActivity.java
@@ -1,15 +1,80 @@
 package photo.tonycai.com.photoapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 
 public class ShowResultActivity extends AppCompatActivity {
+    private int OptionSelectionActivity = 1;
+    private int AfterSaveActivity = 1;
+    public static Bitmap RotateBitmap(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_result);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
+
+        Intent intent = getIntent();
+        String picturePath = intent.getStringExtra("picPath");
+        Boolean filter_a_info = intent.getBooleanExtra("is_filter_a", true);
+        Boolean filter_b_info = intent.getBooleanExtra("is_filter_b", true);
+        Boolean function_a_info = intent.getBooleanExtra("is_function_a", true);
+        Boolean function_b_info = intent.getBooleanExtra("is_function_b", true);
+        int rotation_angle = intent.getIntExtra("rotation_angle", 0);
+        Bitmap imageSource = BitmapFactory.decodeFile(picturePath);
+        ImageFilters filters = new ImageFilters();
+        Bitmap afterFilter = imageSource;
+        if (filter_a_info == true)
+            afterFilter = filters.applyBlackFilter(afterFilter);
+        if (filter_b_info == true)
+            afterFilter = filters.applyHighlightEffect(afterFilter);
+        afterFilter = RotateBitmap(afterFilter, (rotation_angle) * 90);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageBitmap(afterFilter);
+        Button yes = (Button) findViewById(R.id.yes_button);
+        Button no = (Button) findViewById(R.id.no_button);
+        final Bitmap finalAfterFilter = afterFilter;
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaStore.Images.Media.insertImage(getContentResolver(), finalAfterFilter, "title", "description");
+                Intent myIntent = new Intent(ShowResultActivity.this, AfterSaveActivity.class);
+                startActivityForResult(myIntent, AfterSaveActivity);
+            }
+        });
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(ShowResultActivity.this, OptionSelectionActivity.class);
+                startActivityForResult(myIntent, OptionSelectionActivity);
+            }
+        });
+        //CheckBox checkBox_yes = (CheckBox) findViewById(R.id.yes_checkbox);
+        //final CheckBox checkBox_no = (CheckBox) findViewById(R.id.no_checkbox);
+//        if (checkBox_yes.isChecked() == true && checkBox_no.isChecked() != true)
+//            MediaStore.Images.Media.insertImage(getContentResolver(), afterFilter, "title", "description");
+//        else {
+//            if (checkBox_no.isChecked() == true && checkBox_yes.isChecked() != true) {
+//                Intent myIntent = new Intent(ShowResultActivity.this, OptionSelectionActivity.class);
+//                startActivityForResult(myIntent, OptionSelectionActivity);
+//            }
+
 }
+}
+
